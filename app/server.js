071d31e46c9129d05060f80a00e143ef3cea2767
HayashiMitsu <hm20011027@gmail.com>
2021-11-17 01:19:56 +0900

    add:room機能用のjoinメソッドの追加

--- app/server.js
+++ app/server.js
@@ -3,15 +3,30 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+let store = {};
+
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/public/chat.html");
+});
+
 io.on("connection", (socket) => {
+  socket.on("join", (msg) => {
+    usrobj = {
+      'room': msg.roomId,
+      'name': msg.name
+    }
+    store = usrobj;
+    socket.join(msg.roomId);
+  })
+
   socket.on("post", (msg) => {
-    io.emit("room", msg);
+    io.to(store.room).emit("message", msg);
   });
 });
 
