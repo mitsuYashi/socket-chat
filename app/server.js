1609e43453cc4fb9c0578a7dd328594bcf38ee7f
HayashiMitsu <hm20011027@gmail.com>
2021-11-16 18:26:40 +0900

    add:serverサイドのsocket通信

--- app/server.js
+++ app/server.js
@@ -1,5 +1,20 @@
-import express from "express";
-import path from "path";
-import bodyParser from "body-parser";
-
+const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("post", (msg) => {
+    io.emit("room", msg);
+  });
+});
+
+http.listen(3000, () => {
+    console.log("success listen 3000");
+})
